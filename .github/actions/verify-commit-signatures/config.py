6b7db6130162fc0b12693d4ceb5ae864ef7ce603
Nick K <nikita.k@lido.fi>
2026-08-10 14:11:38 +0000

    feat: simplified config

--- .github/actions/verify-commit-signatures/config.py
+++ .github/actions/verify-commit-signatures/config.py
@@ -14,17 +14,7 @@ class RepositoryPolicy:
 REPOSITORIES = {
     # Repositories currently enforced with the SecOps signer group.
     "blacklist-monitoring": RepositoryPolicy(("secops",)),
-    "linters": RepositoryPolicy(("secops",)),
     "tf-drift-mon-scan": RepositoryPolicy(("secops",)),
     "tf-drift-mon-image": RepositoryPolicy(("secops",)),
     "secops_release_sandbox": RepositoryPolicy(("secops",)),
-
-    # Enable after each repository is ready for signature enforcement.
-    # "action-gh-release": RepositoryPolicy(("secops",)),
-    # "check-user-permission": RepositoryPolicy(("secops",)),
-    # "conventional-changelog-action": RepositoryPolicy(("secops",)),
-    # "create-pull-request": RepositoryPolicy(("secops",)),
-    # "gh-find-current-pr": RepositoryPolicy(("secops",)),
-    # "github-pages-action": RepositoryPolicy(("secops",)),
-    # "hadolint-action": RepositoryPolicy(("secops",)),
 }
